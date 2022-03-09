07f478cc33167aebb9f3ddb27834eda6d45d3cbb
Myoung <mhbae93@gmail.com>
2022-03-09 11:48:41 +0000

    created expense tracker banner and graph component wip

---
+++ client/src/components/Graph.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Graph = () => {
+  return (
+    <div className="flex justify-content max-w-xs mx-auto">
+      <div className="item">
+        <div className="chart relative">Chart</div>
+        <div className="flex flex-col py-10 gap-4"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Graph;
