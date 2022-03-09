e94d9c20a2f6bfb50068801e4417115eaee5d34b
Myoung <mhbae93@gmail.com>
2022-03-09 12:49:22 +0000

    added dependancies for chart.js and created a doughnut chart

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -1,14 +1,42 @@
 import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement } from "chart.js";
 
-const Graph = () => {
+ChartJS.register(ArcElement);
+
+const config = {
+  data: {
+    datasets: [
+      {
+        data: [300, 50, 100],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+        borderRadius: 30,
+        spacing: 10,
+      },
+    ],
+  },
+  options: {
+    cutout: 115,
+  },
+};
+
+export default function Graph() {
   return (
     <div className="flex justify-content max-w-xs mx-auto">
       <div className="item">
-        <div className="chart relative">Chart</div>
+        <div className="chart relative">
+          <Doughnut {...config}></Doughnut>
+          <h3 className="mb-4 font-bold graph-title">Total
+          <span className="block text-3xl text-emerald-400">£{0}</span>
+          </h3>
+        </div>
         <div className="flex flex-col py-10 gap-4"></div>
       </div>
     </div>
   );
-};
-
-export default Graph;
+}
