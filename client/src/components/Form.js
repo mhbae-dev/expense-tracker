fc9bfcead69e7a6b56a791e67d0c2e21ebcf0692
Myoung <mhbae93@gmail.com>
2022-03-09 14:00:46 +0000

    added react-hook-form to work with form inputs

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,19 +1,28 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Form = () => {
+  const { register, handleSubmit, resetField } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="form max-w-sm mx-auto w-96">
       <h1 className="font-bold pb-4 text-xl">Transcation</h1>
-      <form id="form">
+
+      <form id="form" onSubmit={handleSubmit(onSubmit)}>
         <div className="grid gap-4">
           <div className="input-group">
             <input
               type="text"
               placeholder="Salary, Rent, SIP"
               className="form-input"
+              {...register("name")}
             />
           </div>
-          <select className="form-input">
+          <select className="form-input" {...register("type")}>
             <option value="Investment" defaultValue>
               Investment
             </option>
@@ -25,7 +34,12 @@ const Form = () => {
             </option>
           </select>
           <div className="input-group">
-            <input type="text" placeholder="Amount" className="form-input" />
+            <input
+              type="text"
+              placeholder="Amount"
+              className="form-input"
+              {...register("amount")}
+            />
           </div>
           <div className="submit-btn">
             <button className="border py-2 text-white bg-indigo-500 w-full">
